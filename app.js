a985d5d652b8f70a40c13f59a7598a799a408c7a
sebnun <sebnun@gmail.com>
2016-09-10 22:18:37 -0300

    all use cases implemented

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const multer = require('multer');
+
+const app = express();
+
+const upload = multer({ dest: 'uploads/' });
+
+app.use('/', express.static('public'));
+
+app.post('/get-file-size', upload.single('file'), (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/json' });
+  res.end(JSON.stringify({ size: req.file.size }));
+});
+
+app.listen(8084);
